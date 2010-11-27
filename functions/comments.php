cc60b07b64ca07f894c3634393ad7b637a7a8eb8
mike <muffinger@gmail.com>
2010-11-27 20:53:05 +1100

    oops.. typo in the POST

--- functions/comments.php
+++ functions/comments.php
@@ -3,7 +3,7 @@ require_once('/home/life/public_html/classes/comments.php');
 
 $comments = new Comments;
 
-if (isset($_POST['add_comment'])) {	
+if (isset($_POST['post_comment'])) {	
 	$userid = (int)$_SESSION['userid'];
 	$pageid = (int)$_POST['pageid'];
 	$content = $_POST['content']; // secure
