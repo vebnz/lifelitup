091c43a9ec4f8169877541ff8254f16245f9d5a8
mike <muffinger@gmail.com>
2010-11-27 18:42:34 +1100

    added a join query to get username of comment poster

--- classes/comments.php
+++ classes/comments.php
@@ -5,8 +5,9 @@ class Comments {
 	function show($page_id) {
 		$db = Database::obtain();
 
-		$sql = "SELECT id, user_id, content, date_posted
+		$sql = "SELECT user_id, content, date_posted, username
 			FROM " . tbl_comments . "
+			JOIN " . tbl_users . " ON " . tbl_comments . ".user_id = " . tbl_users . ".id
 			WHERE page_id = " . (int)$page_id;
 		$rows = $db->fetch_array($sql);
 
