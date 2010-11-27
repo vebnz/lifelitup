d2ad563263071f8fa1b378319d40b76a1875c371
mike <muffinger@gmail.com>
2010-11-27 20:59:34 +1100

    making sure people who are not logged in cannot post a new comment

--- functions/comments.php
+++ functions/comments.php
@@ -3,7 +3,17 @@ require_once('/home/life/public_html/classes/comments.php');
 
 $comments = new Comments;
 
-if (isset($_POST['post_comment'])) {	
+if (isset($_POST['post_comment'])) {
+
+	if (!$auth) {
+		die('cannot auth');
+	}
+
+	if (!$auth->isLoggedIn()) {
+		$msg = "Sorry, you're not logged in, so you can't post a new comment.";
+		return;
+	}
+	
 	$userid = (int)$_SESSION['userid'];
 	$pageid = (int)$_POST['page_id'];
 	$content = $_POST['content']; // secure
