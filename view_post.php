d2956eefddc89b9f6f081e37aa64ac7a0871b29e
mike <muffinger@gmail.com>
2010-11-27 20:51:08 +1100

    passed the pagess id to the template

--- view_post.php
+++ view_post.php
@@ -14,7 +14,7 @@ $show = $blog->showPost($_GET['id']);
 $show_comments = $comments->show($_GET['id']);
 
 $template = $twig->loadTemplate('view_post.html');
-echo $template->render(array('title' => 'Lifelitup', 'posts' => $show, 'comments' => $show_comments));
+echo $template->render(array('page_id' => $_GET['id'], 'posts' => $show, 'comments' => $show_comments));
 
 require_once('footer.php');
 $db->close();
