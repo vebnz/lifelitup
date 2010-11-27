0b860429e1e6d64efad0cedd639e46cdaa5ba424
mike <muffinger@gmail.com>
2010-11-27 21:13:56 +1100

    added table so we can use comments for different modules

--- classes/comments.php
+++ classes/comments.php
@@ -2,13 +2,14 @@
 
 class Comments {
 
-	function show($page_id) {
+	function show($table, $page_id) {
 		$db = Database::obtain();
 
 		$sql = "SELECT user_id, content, date_posted, username
 			FROM " . tbl_comments . "
 			JOIN " . tbl_users . " ON " . tbl_comments . ".user_id = " . tbl_users . ".id
-			WHERE page_id = " . (int)$page_id;
+			WHERE page_id = " . (int)$page_id . "
+			AND table = " . $table;
 		$rows = $db->fetch_array($sql);
 
 		return $rows;	
