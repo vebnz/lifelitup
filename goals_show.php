47e864c1a79f9fa40c7b53b3dbc10e0e13eac632
mike <muffinger@gmail.com>
2010-11-27 21:12:47 +1100

    added comment code for showing goals

--- goals_show.php
+++ goals_show.php
@@ -10,15 +10,17 @@ require_once('header.php');
 require_once('functions/authenticate.php');
 require_once('functions/todo.php');
 require_once('functions/goals.php');
+require_once('functions/comments.php');
 
 if (!$auth->isLoggedIn()) {
 	header("Location: login.php");
 }
 
 $show = $goals->show($_GET['id']);
+$show_comments = $comments->show($_GET['id']);
 
 $template = $twig->loadTemplate('goals_show.html');
-echo $template->render(array('goal' => $show, 'username' => $_SESSION['username'], 'msg' => $msg));
+echo $template->render(array('page_id' => $_GET['id'], 'goal' => $show, 'comments' => $show_comments, 'username' => $_SESSION['username'], 'msg' => $msg));
 
 require_once('footer.php');
 $db->close();
