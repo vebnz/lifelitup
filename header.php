a42d0bd4e1fcc33294b98d8b63a55d77beaaa2b6
mike <muffinger@gmail.com>
2010-12-28 10:03:35 +1100

    commented out pages bit.. Im not happy with it so far, its pretty shit and I believe very inefficient

--- header.php
+++ header.php
@@ -9,8 +9,9 @@ if ($auth->isLoggedIn()) {
 	$logged_in = 0;
 }
 
-$showPages = $pages->createNavbar($logged_in);
+/*$showPages = $pages->createNavbar($logged_in);
 
 $template = $twig->loadTemplate('header.html');
 echo $template->render(array('title' => 'LifeLitUp', 'pages' => $showPages));
+*/
 ?>
