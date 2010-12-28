856133d54bfdb36996829a0566cba69099263a80
mike <muffinger@gmail.com>
2010-12-28 12:57:32 +1100

    passed username into template

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once('functions/blog.php');
 $show = $blog->showAll();
 
 $template = $twig->loadTemplate('blog.html');
-echo $template->render(array('posts' => $show));
+echo $template->render(array('posts' => $show, 'username' => $username));
 
 require_once('footer.php');
 $db->close();
