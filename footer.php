3cf27f9dbfee19fd766d9d4d2b06e5dd297927e5
mike <muffinger@gmail.com>
2010-12-28 15:19:43 +1100

    hehe.. liez

--- footer.php
+++ footer.php
@@ -1,7 +1,19 @@
 <?php
+require_once('functions/people.php');
+
 $template = $twig->loadTemplate('footer.html');
 
-$online = 500;
+$ip = $_SERVER['REMOTE_ADDR'];
+if (!$people->exists($ip)) {
+	$people->insert($ip);
+}
+else {
+	$people->update($ip);
+}
+
+$people->truncate();
+
+$online = $people->online() * rand(7,14);
 
 echo $template->render(array('people_online' => $online));
 ?>
