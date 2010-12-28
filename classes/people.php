f3186c018bc3bf5441b96414d94c329824f19346
mike <muffinger@gmail.com>
2010-12-28 14:40:52 +1100

    oops!

--- classes/people.php
+++ classes/people.php
@@ -8,12 +8,21 @@ class People {
 		$sql = "SELECT 1 FROM " . tbl_people . " WHERE ip = " . ip2long($ip);
 		$row = $db->query_first($sql);
 
-		if (!$empty($row)) {
+		if (!empty($row)) {
 			return true;
 		}
 		else {
 			return false;
 		}
+	}
+
+	function online() {
+		$db = Database::obtain();
+
+		$sql = "SELECT COUNT(*) as counted FROM " . tbl_people;
+		$row = $db->query_first($sql);
+
+		return $row['counted'];
 	}
 
 	function insert($ip) {
@@ -31,13 +40,13 @@ class People {
 		$db = Database::obtain();
 	
 		$data['time'] = "NOW()";
-		$db->update(tbl_people, "ip=" . ip2long($ip));
+		$db->update(tbl_people, $data, "ip=" . ip2long($ip));
 	}
 
 	function truncate() {
 		$db = Database::obtain();
 
-		$sql = "DELETE FROM " . tbl_people . " WHERE time < SUBTIME(NOW(),'0 0:10:0')");
+		$sql = "DELETE FROM " . tbl_people . " WHERE time < SUBTIME(NOW(),'0 0:10:0')";
 		$db->query($sql);
 	}
 
