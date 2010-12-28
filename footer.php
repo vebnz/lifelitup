b66694ca871aa8c5362e1deb2a218271bbac54c1
mike <muffinger@gmail.com>
2010-12-28 14:25:19 +1100

    isd

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <?php
 $template = $twig->loadTemplate('footer.html');
 
-echo $template->render(array('people_online' => 500));
+$online = 500;
+
+echo $template->render(array('people_online' => $online));
 ?>
