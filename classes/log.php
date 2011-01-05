d53599b6488063bf40f0188d041d75e3ba9cafb2
Kingy <k1hosting@gmail.com>
2011-01-05 20:05:02 +1300

    anal coding

--- classes/log.php
+++ classes/log.php
@@ -40,15 +40,15 @@ class Log
 	{
 		if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
 		{
-			$ip=$_SERVER['HTTP_CLIENT_IP'];
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
 		}
 		elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
 		{
-			$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 		}
 		else
 		{
-			$ip=$_SERVER['REMOTE_ADDR'];
+			$ip = $_SERVER['REMOTE_ADDR'];
 		}
 		return $ip;
 	}
