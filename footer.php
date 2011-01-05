0a5dd509fbfb78303bcb0149c664a8b3b98f07b7
vebtor <muffinger@gmail.com>
2011-01-05 20:23:51 +1300

    on localhost, the IP will appear something like ::0 so I have taken steps to rectify this

--- footer.php
+++ footer.php
@@ -5,12 +5,14 @@ $template = $twig->loadTemplate('footer.html');
 
 $ip = getIP();
 
-
-if (!$people->exists($ip)) {
-    $people->insert($ip);
-}
-else {
-    $people->update($ip);
+// make sure IP is right before doing anything with it...
+if (preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/', $ip)) {
+	if (!$people->exists($ip)) {
+    	$people->insert($ip);
+	}
+	else {
+    	$people->update($ip);
+	}
 }
 
 $people->truncate();
