7519d5b2bb6818523417be2f7a7249de515668fb
vebtor <muffinger@gmail.com>
2011-01-05 19:57:57 +1300

    $ip wont work on localhost for users online

--- footer.php
+++ footer.php
@@ -4,11 +4,14 @@ require_once('functions/people.php');
 $template = $twig->loadTemplate('footer.html');
 
 $ip = $_SERVER['REMOTE_ADDR'];
-if (!$people->exists($ip)) {
-	$people->insert($ip);
-}
-else {
-	$people->update($ip);
+
+if ($ip > 0) {
+    if (!$people->exists($ip)) {
+            $people->insert($ip);
+    }
+    else {
+            $people->update($ip);
+    }
 }
 
 $people->truncate();
