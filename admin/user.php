a6a05acb1c92268fb872e7c464e67549e1f30e1e
vebtor <muffinger@gmail.com>
2011-01-06 15:24:24 +1300

    changed some stuff around

--- admin/user.php
+++ admin/user.php
@@ -8,7 +8,7 @@ $db->connect();
 
 require_once('functions/admin/user.php');
 require_once('functions/authenticate.php');
-require_once('includes/TableGear/include/TableGear1.6.1.php');
+require_once('includes/TableGear1.6.1.php');
 
 if (!$auth->isLoggedIn()) {
 	echo '<meta http-equiv="refresh" content="0;url=../login.php" />';
@@ -19,8 +19,8 @@ $options["database"]["table"] = "tbl_users";
 $table = new TableGear($options);
 ?>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript" src="../includes/TableGear/javascripts/TableGear1.6.1-jQuery.js"></script>
-<link type="text/css" rel="stylesheet" href="../includes/TableGear/stylesheets/tablegear.css" />
+<script type="text/javascript" src="../includes/TableGear1.6.1-jQuery.js"></script>
+<link type="text/css" rel="stylesheet" href="../includes/tablegear.css" />
 <?
 echo $table->getTable();
 echo $table->getJavascript("jquery");
