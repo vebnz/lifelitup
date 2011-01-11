4e7cf3d5b85b59c1a5e4a254665987cba6f631d2
vebtor <muffinger@gmail.com>
2011-01-11 18:22:02 +1300

    fixed some admin stuff, and changing auth system  not to use username but email instead

---
+++ admin/goal.php
@@ -0,0 +1,30 @@
+<?php
+//die('Admin stuff disabled.. let\'s get basic functionality added before moving on to \'behind the scenes\' stuff');
+chdir('../');
+require_once('includes/config.php');
+require_once('database/db.php');
+
+$db = Database::obtain(DB_SERVER, DB_USER, DB_PASS, DB_DATABASE);
+$db->connect();
+
+require_once('functions/admin/user.php');
+require_once('functions/authenticate.php');
+require_once('includes/TableGear1.6.1.php');
+
+if (!$auth->isLoggedIn()) {
+	echo '<meta http-equiv="refresh" content="0;url=../login.php" />';
+	die;
+}
+
+$options["database"]["table"] = tbl_goals;
+$table = new TableGear($options);
+?>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" src="../includes/TableGear1.6.1-jQuery.js"></script>
+<link type="text/css" rel="stylesheet" href="../includes/tablegear.css" />
+<?php
+echo $table->getTable();
+echo $table->getJavascript("jquery");
+
+$db->close();
+?>
