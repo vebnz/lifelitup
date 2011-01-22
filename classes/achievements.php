fc4f27abbdf87b8971ecde71a789d41a90ca8253
Kingy <k1hosting@gmail.com>
2011-01-22 22:28:32 +1300

    achievement listings in user profile

--- classes/achievements.php
+++ classes/achievements.php
@@ -11,6 +11,20 @@ class Achievements {
 
 	}
 	
+	function getAchievements($userid) {
+		$db = Database::obtain();
+		
+		$sql = "SELECT " . tbl_goals . ".id, " . tbl_goals . ".name as name, " . tbl_goals . ".icon as icon
+			FROM " . tbl_achievements . "
+			JOIN " . tbl_goals . " ON " . tbl_achievements . ".goal_id = " . tbl_goals . ".id
+			JOIN " . tbl_users . " ON " . tbl_achievements . ".user_id = " . tbl_users . ".id
+			WHERE " . tbl_users . ".id = " . (int)$userid;
+			
+		$achievements = $db->fetch_array($sql);
+		
+		return $achievements;
+	}
+	
 	function image_file_extension($filename)
 	{
 		return end(explode(".", $filename));
--- profile.php
+++ profile.php
@@ -8,16 +8,17 @@ $db->connect();
 require_once('twig/twig.php');
 require_once('header.php');
 require_once('functions/authenticate.php');
-
+require_once('functions/achievements.php');
+require_once('functions/profile.php');
 if (!$auth->isLoggedIn()) {
 	header("Location: login.php");
 	die;
 }
 
-require_once('functions/profile.php');
+$achievements = $achievement->getAchievements($_SESSION['userid']);
 
 $template = $twig->loadTemplate('profile.html');
-echo $template->render(array('msg' => $msg, 'profile' => $profileArr));
+echo $template->render(array('msg' => $msg, 'profile' => $profileArr, 'achievements' => $achievements));
 
 $db->close();
 
