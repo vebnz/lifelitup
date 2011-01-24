07366a96f2da06e3812be0fe4c55b1fbc000d78b
vebtor <muffinger@gmail.com>
2011-01-24 18:51:06 +1300

    made it so you can view someones profile

--- functions/profile.php
+++ functions/profile.php
@@ -4,6 +4,7 @@ require_once('classes/profile.php');
 $profile = new Profile;
 
 if (isset($_POST['profile_edit'])) {
+	// is user allowed to edit this data?
 	$data['first_name'] = $_POST['first_name']; // secure
 	$data['last_name'] = $_POST['last_name']; // secure
 	$data['facebook'] = $_POST['facebook']; // secure
@@ -13,5 +14,4 @@ if (isset($_POST['profile_edit'])) {
 	$msg = 'You have updated your profile.';
 }
 
-$profileArr = $profile->get($_SESSION['userid']);
 ?>
--- profile.php
+++ profile.php
@@ -11,16 +11,29 @@ require_once('functions/authenticate.php');
 require_once('functions/achievements.php');
 require_once('functions/friends.php');
 require_once('functions/profile.php');
+
 if (!$auth->isLoggedIn()) {
 	header("Location: login.php");
 	die;
 }
 
-$achievements = $achievement->getAchievements($_SESSION['userid']);
-$friends = $friends->getFriends($_SESSION['userid']);
+if (isset($_GET['userid'])) {
+	$userid = intval($_GET['userid']);
+}
+else {
+	$userid = $_SESSION['userid'];
+}
+
+if ($userid != $_SESSION['userid']) {
+	$viewing = true;
+}
+
+$achievements = $achievement->getAchievements($userid);
+$friends = $friends->getFriends($userid);
+$profileArr = $profile->get($userid);
 
 $template = $twig->loadTemplate('profile.html');
-echo $template->render(array('msg' => $msg, 'profile' => $profileArr, 'achievements' => $achievements, 'friends' => $friends));
+echo $template->render(array('msg' => $msg, 'profile' => $profileArr, 'achievements' => $achievements, 'friends' => $friends, 'viewing' => $viewing));
 
 $db->close();
 
