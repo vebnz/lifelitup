c691526bcf9c6b6a80655b5495b094930d92ccf4
vebtor <muffinger@gmail.com>
2011-01-27 12:55:29 +1300

    added PunBB forum... now we need to make login/register/profile use LLUs

---
+++ forum/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = '175.107.158.214';
+$db_name = 'life_forum';
+$db_username = 'life';
+$db_password = '2vc44rb5';
+$db_prefix = '';
+$p_connect = false;
+
+$base_url = 'http://localhost/llu/forum';
+
+$cookie_name = 'forum_cookie_1ecddb';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+
+define('FORUM', 1);
