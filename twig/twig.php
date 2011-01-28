8dde0d92e27db61fd2ff40a4ccf8954d576baf90
vebtor <muffinger@gmail.com>
2011-01-28 14:37:49 +1300

    s

--- twig/twig.php
+++ twig/twig.php
@@ -7,4 +7,7 @@ $loader = new Twig_Loader_Filesystem(current_theme);
 $twig = new Twig_Environment($loader);
 $twig->addExtension(new Twig_Extension_Text());
 
+require_once('people_online.php');
+$twig->addGlobal('userid', $_SESSION['userid']);
+
 ?>
