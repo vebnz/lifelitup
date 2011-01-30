6ddfcd600bddf7886684625dca1e2fa642b58061
vebtor <muffinger@gmail.com>
2011-01-30 16:34:06 +1300

    removing whitespace from email verification

--- classes/authenticate.php
+++ classes/authenticate.php
@@ -85,7 +85,7 @@ class Authenticate {
 			return $msg;
 		}
 
-		$data['code'] = trim(shell_exec('head -c 16 < /dev/urandom'));
+		$data['code'] = bin2hex(shell_exec('head -c 16 < /dev/urandom'));
 		$data['email'] = $email;
 		$data['password'] = $hasher->HashPassword($password);
 
