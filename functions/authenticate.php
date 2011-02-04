20bdbc009b758a26847bb3250ecb7e834edb3879
Kingy <k1hosting@gmail.com>
2011-02-05 09:56:20 +1300

    forgot password

--- functions/authenticate.php
+++ functions/authenticate.php
@@ -227,6 +227,8 @@ if (isset($_POST['resetSubmit'])) {
 	if ($auth->checkCode($userid, $code)) {
 		$isConfirmed = 1;
 		$auth->resetPassword($userid, $password);
+		$msg = 'Your password has now been reset. You can now login!.';
+		header("Location: login.php");
 	}
 	else {
 		$msg = 'Confirmation code is wrong. <a href=\"#\">Click here to recieve another one.</a>';
